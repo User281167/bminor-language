7c575e4f2739272438703f3de1ff97da37e40047
Juan <juandiego.velez@utp.edu.co>
2025-10-25 00:37:12 -0500

    refactor: remover errores no usados

--- semantic/semantic_error.py
+++ semantic/semantic_error.py
@@ -2,10 +2,13 @@ from enum import Enum
 
 
 class SemanticError(Enum):
-    MISMATCHING_TYPES = "Mismatching types"
+    UNKNOWN = "Unknown error"
+
     MISMATCH_ASSIGNMENT = "Mismatch assignment"
     MISMATCH_DECLARATION = "Mismatch declaration"
-    MISMATCH_FUNCTION_CALL = "Mismatch function call"
+
+    UNDECLARED_VARIABLE = "Undeclared variable"
+    UNDECLARED_ARRAY = "Undeclared array"
 
     VOID_VARIABLE = "Void variable"
     VOID_ARRAY = "Void array"
@@ -14,9 +17,14 @@ class SemanticError(Enum):
     ARRAY_SIZE_MUST_BE_INTEGER = "Array size must be integer"
     ARRAY_SIZE_MUST_BE_POSITIVE = "Array size must be positive"
     ARRAY_SIZE_MISMATCH = "Array size mismatch"
+    ARRAY_INDEX_MUST_BE_INTEGER = "Array index must be integer"
+    ARRAY_EXPECTED = "Array expected"
+    MULTI_DIMENSIONAL_ARRAYS = "Multi-dimensional arrays are not supported"
+    ARRAY_NOT_SUPPORTED_SIZE = (
+        "Array not supported size in function parameters or return type"
+    )
 
-    INDEX_MUST_BE_INTEGER = "Index must be integer"
-    INDEX_MUST_BE_POSITIVE = "Index must be positive"
+    INDEX_MUST_BE_POSITIVE = "Index must be integer positive"
     INDEX_OUT_OF_BOUNDS = "Index out of bounds"
 
     INVALID_UNARY_OP = "Invalid unary operator"
@@ -24,15 +32,10 @@ class SemanticError(Enum):
 
     UNARY_OP_TYPE = "Unary operator type"
     BINARY_OP_TYPE = "Binary operator type"
+    BINARY_ARRAY_OP = "Binary array operator"
 
-    UNKNOWN = "Unknown error"
-
-    INMUTABLE_ASSIGNMENT = "Inmutable assignment"
-    UNDECLARED_VARIABLE = "Undeclared variable"
-    UNDECLARED_FUNCTION = "Undeclared function"
-    UNDECLARED_TYPE = "Undeclared type"
-    UNDECLARED_ARRAY = "Undeclared array"
-    UNDECLARED_ARRAY_TYPE = "Undeclared array type"
+    INVALID_INCREMENT = "Invalid increment"
+    INVALID_DECREMENT = "Invalid decrement"
 
     REDEFINE_VARIABLE_TYPE = "Redeclare variable type"
     REDEFINE_VARIABLE = "Redeclare variable"
@@ -43,41 +46,24 @@ class SemanticError(Enum):
     REDEFINE_PARAMETER_TYPE = "Redeclare parameter type"
     REDEFINE_PARAMETER = "Redeclare parameter"
 
-    ARRAY_EXPECTED = "Array expected"
+    IF_CONDITION_MUST_BE_BOOLEAN = "If condition must be boolean"
 
-    MULTI_DIMENSIONAL_ARRAYS = "Multi-dimensional arrays are not supported"
-
-    VOID_PARAMETER = "Void parameter"
-
-    ARRAY_INDEX_MUST_BE_INTEGER = "Array index must be integer"
-
-    ARRAY_NOT_SUPPORTED_SIZE = (
-        "Array not supported size in function parameters or return type"
-    )
-
-    IS_NOT_FUNCTION = "Is not function"
-    UNDEFINED_FUNCTION = "Undefined function"
-    FUNCTION_CALL_ARGUMENT_MISMATCH = "Function call argument mismatch"
-    WRONG_NUMBER_OF_ARGUMENTS = "Wrong number of arguments"
-    MISMATCH_ARGUMENT_TYPE = "Mismatch argument type"
-
-    RETURN_TYPE_MISMATCH = "Return type mismatch"
-    RETURN_OUT_OF_FUNCTION = "Return out of function"
-    RETURN_TYPE_NOT_SUPPORTED = "Return type not supported"
-    RETURN_IN_VOID_FUNCTION = "Return in void function"
-
+    LOOP_CONDITION_MUST_BE_BOOLEAN = "Loop condition must be boolean"
     BREAK_OUT_OF_LOOP = "Break out of loop"
     CONTINUE_OUT_OF_LOOP = "Continue out of loop"
 
+    FUNCTION_USED_AS_VALUE = "Function used as value"
+    UNDEFINED_FUNCTION = "Undefined function"
+    IS_NOT_FUNCTION = "Is not function"
+
+    WRONG_NUMBER_OF_ARGUMENTS = "Wrong number of arguments"
+    MISMATCH_ARGUMENT_TYPE = "Mismatch argument type"
+
+    VOID_PARAMETER = "Void parameter"
+
+    RETURN_TYPE_MISMATCH = "Return type mismatch"
+    RETURN_OUT_OF_FUNCTION = "Return out of function"
+    RETURN_IN_VOID_FUNCTION = "Return in void function"
+
     PRINT_VOID_EXPRESSION = "Print void expression"
     PRINT_ARRAY_NOT_ALLOWED = "Cannot print array"
-
-    IF_CONDITION_MUST_BE_BOOLEAN = "If condition must be boolean"
-
-    BINARY_ARRAY_OP = "Binary array operator"
-
-    LOOP_CONDITION_MUST_BE_BOOLEAN = "Loop condition must be boolean"
-
-    INVALID_INCREMENT = "Invalid increment"
-    INVALID_DECREMENT = "Invalid decrement"
-    FUNCTION_USED_AS_VALUE = "Function used as value"
