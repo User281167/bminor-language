8d657f7c5fd887b9c84cda174e13e6a8c65f0a34
Juan <juandiego.velez@utp.edu.co>
2025-10-31 10:28:42 -0500

    fix: check en auto array

--- semantic/checker.py
+++ semantic/checker.py
@@ -1140,18 +1140,7 @@ class Check(Visitor):
                 SemanticError.UNDECLARED_ARRAY,
             )
             return
-        elif isinstance(load_arr, AutoDecl):
-            pass
-        # Dentro de una función se puede usar un array como parámetro comprobar que el parámetro es un array
-        elif isinstance(load_arr, Param) and not isinstance(load_arr.type, ArrayType):
-            self._error(
-                f"{n.array.name!r} is not an array",
-                n.lineno,
-                SemanticError.ARRAY_EXPECTED,
-            )
-            return
-        # si no es param array deber ser una declaración
-        elif not isinstance(load_arr, Param) and not isinstance(load_arr, ArrayDecl):
+        elif not isinstance(load_arr.type, ArrayType):
             self._error(
                 f"{n.array.name!r} is not an array",
                 n.lineno,
